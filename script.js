eb3eee1f5d2eba962f8055e256d7e0e9a60e3394
Jupiter Dev <141870656+TheJupiterDev@users.noreply.github.com>
2025-03-03 09:42:06 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.onload = function() {
     setProgress('progress1', 18);
     setProgress('progress2', 35);
     setProgress('progress3', 60);
-    setProgress('progress4', 8);
+    setProgress('progress4', 0);
 }
 function showPage(pageId) {
     const sections = document.querySelectorAll(".content section");
